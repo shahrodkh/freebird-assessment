455b986481390f42badd55bb7ade31d5f3f1faad
shahrodkh <shahrodkh@gmail.com>
2017-04-20 20:49:57 -0700

    better readability

--- controllers/offerController.js
+++ controllers/offerController.js
@@ -1,26 +1,28 @@
 const Offer = require('../models/offerModel');
 
-function createOffer(req, res) {
-  Offer.create(req.payload, (err, offer) => {
-    if (err) return console.error(err);
-    res('Posted offer ' + JSON.stringify(offer));
+function createOffer(request, reply) {
+  const newOffer = new Offer(request.payload);
+  newOffer.save((err, offer) => {
+    if (err) return reply(err);
+    reply(offer).code(200);
   });
 }
 
-function deleteOffer(req, res) {
-  Offer.remove({ id: encodeURIComponent(req.params.id) }, (err, offer) => {
-    if (err) return console.error(err);
-    res('Deleted offer ' + JSON.stringify(offer));
+function deleteOffer(request, reply) {
+  const id = encodeURIComponent(request.params.id);
+  Offer.remove({ id }, (err) => {
+    if (err) return reply(err);
+    reply().code(200);
   });
 }
 
-function getOffers(req, res) {
-  const limit = req.params.limit ? 
-    { limit : parseInt(encodeURIComponent(req.params.limit), 10) } :
+function getOffers(request, reply) {
+  const limit = request.params.limit ? 
+    { limit : parseInt(encodeURIComponent(request.params.limit), 10) } :
     null;
   Offer.find({}, null, limit, (err, offers) => {
-    if (err) return console.error(err);
-    res(offers);
+    if (err) return reply(err);
+    reply(offers).code(200);
   })
 }
 
